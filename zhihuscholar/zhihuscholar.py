3e1182e203bac30bcf4a48455aa2357e6d95fddf
yhylord <yhylord@gmail.com>
2017-09-25 21:38:10 +0800

    Add connection to database

--- zhihuscholar/zhihuscholar.py
+++ zhihuscholar/zhihuscholar.py
@@ -1,12 +1,21 @@
 from flask import Flask
+import sqlite3
 
 
 app = Flask(__name__)
 app.config.from_object('config')
 
+
 @app.route('/')
 def hello():
     return "Hello, world!"
+
+
+def connect_db():
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+
 
 if __name__ == '__main__':
     app.run()
