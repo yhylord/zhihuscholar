426d283501f0bd35e8d7ce525e1b8f5af69089ae
yhylord <yhylord@gmail.com>
2017-09-25 22:03:50 +0800

    Package zhihuscholar

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name='zhihuscholar',
+    packages=['zhihuscholar'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
---
+++ zhihuscholar/__init__.py
@@ -0,0 +1,1 @@
+from .zhihuscholar import app
