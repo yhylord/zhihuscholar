b500fa4e069953d7792a668d8eb9cd7bda5d7109
yhylord <yhylord@gmail.com>
2017-10-17 21:40:34 +0800

    Add login authentication

--- zhihuscholar/__init__.py
+++ zhihuscholar/__init__.py
@@ -1,10 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-app.config.from_object('config')
+app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+
 login_manager = LoginManager()
 login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
 from zhihuscholar import views
--- zhihuscholar/views.py
+++ zhihuscholar/views.py
@@ -1,5 +1,6 @@
-from flask import render_template, redirect
-from zhihuscholar import app, login_manager
+from flask import session, render_template, redirect, url_for
+from flask_login import login_user
+from zhihuscholar import app, bcrypt, login_manager
 from .forms import LoginForm
 from .models import User
 
@@ -10,11 +11,19 @@ def index():
     return render_template('index.html', title='Home')
 
 
-@app.route('/login', methods=['get', 'post'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        return redirect('/')
+        email = form.email.data
+        registered_user = User.query.filter_by(email=email).first()
+        if registered_user:
+            password_hash = registered_user.password
+            password = form.password.data
+            if bcrypt.check_password_hash(password_hash, password):
+                login_user(registered_user)
+                session['remember_me'] = form.remember_me.data
+                return redirect(url_for('index'))
     return render_template('login.html', title='Login', form=form)
 
 
